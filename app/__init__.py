c332e63328e9a588e5f6418663070d0f7ea2bfb8
acmp17 <alliewxy@gmail.com>
2022-04-29 13:36:18 -0400

    04/29 commits: hopefully pytest works for now

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from flask_wtf.csrf import CSRFProtect
 
 from app.auth import auth
 from app.auth import auth
-from app.cli import create_database, create_log_folder
+from app.cli import create_database
 from app.context_processors import utility_text_processors
 from app.db import db
 from app.db.models import User
@@ -42,11 +42,9 @@ def create_app():
     # these load functions with web interface
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
-    app.register_blueprint(database)
     # these load functionality without a web interface
     app.register_blueprint(log_con)
     app.register_blueprint(error_handlers)
-    app.register_blueprint(songs)
     app.context_processor(utility_text_processors)
     # add command function to cli commands
     app.cli.add_command(create_database)
